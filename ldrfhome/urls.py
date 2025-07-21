150b3b73e8b173ce57db79b9a695dbd73d2b8332
Alhadji Oumate <alhadjioum0@gmail.com>
2025-07-21 11:01:54 +0100

    feat: testing serializer with routes

--- ldrfhome/urls.py
+++ ldrfhome/urls.py
@@ -41,7 +41,7 @@ router.register(r'groups', views.GroupViewSet)
 # router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls'))
 ]
